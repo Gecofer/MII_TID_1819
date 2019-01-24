6a6d40fb1196d2afc21827888a7cbce2f2d186a1
Gecofer <gema.cofer95@gmail.com>
2019-01-25 00:14:56 +0100

    Fichero para librerías

--- librerias.R
+++ librerias.R
@@ -4,29 +4,15 @@ library(magrittr)
 library(plyr)
 library(RColorBrewer)
 library(ggplot2) 
-
-
-# Paquete para minería de datos, permite procesar datos de tipo texto
-# El paquete tm, necesita el paquete NLP
-library(NLP)
+library(NLP) # Paquete para minería de datos, permite procesar datos de tipo texto
 library(tm)
-
-# Paquete para minería de datos, agrupa aquellos términos que contienen la misma raíz
-library(SnowballC)
-
+library(SnowballC) # Paquete para minería de datos, agrupa aquellos términos que contienen la misma raíz
 library(wesanderson)
-
-library(devtools)                   # hace falta esta librería para que funcione
-# install_github("mukul13/rword2vec") # nos instalamos la libreria desde Github
-library(rword2vec)
-
+library(devtools)               
+library(rword2vec) # install_github("mukul13/rword2vec")
 library(rlist)
-
 library(quanteda) 
-
 library(tseries)
 library(wordcloud)
-
 library(dplyr)
-
 library(ROCR)

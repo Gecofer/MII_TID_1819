e89f406233e7191358cc3167db4e5d9a76bd14c0
Gecofer <gema.cofer95@gmail.com>
2019-01-29 10:26:22 +0100

    Librerias

---
+++ librerias.R
@@ -0,0 +1,40 @@
+# Cargar todas las librerías antes de ejecutar
+
+library(arules)
+library(arulesViz)
+library(caret)
+library(dbscan)
+library(devtools)
+library(dplyr)
+library(e1071)
+library(ggplot2) 
+library(magrittr)
+library(NLP) # Paquete para minería de datos, permite procesar datos de tipo texto
+library(plyr)
+library(proxy)
+library(quanteda)
+library(RColorBrewer)
+library(rlist)
+library(ROCR)
+library(rword2vec) # install_github("mukul13/rword2vec")
+library(RTextTools)
+library(SnowballC) # Paquete para minería de datos, agrupa aquellos términos que contienen la misma raíz
+library(tidytext)
+library(tm)
+library(tseries)
+library(wesanderson)
+library(wordcloud)
+
+
+
+
+
+
+
+            
+
+
+
+
+
+

517636e587ca203a92811bf29aa189577d94f6b6
Luis Gallego Quero <lgaq94@gmail.com>
2019-01-29 11:59:52 +0100

    mas manteca

--- librerias.R
+++ librerias.R
@@ -7,16 +7,20 @@ library(dbscan)
 library(devtools)
 library(dplyr)
 library(e1071)
+library(factoextra)
 library(ggplot2) 
+library(ggpubr)
 library(magrittr)
 library(NLP) # Paquete para minería de datos, permite procesar datos de tipo texto
 library(plyr)
 library(proxy)
 library(quanteda)
+library(randomForest)
+library(rattle)
 library(RColorBrewer)
 library(rlist)
 library(ROCR)
-library(rword2vec) # install_github("mukul13/rword2vec")
+library(rword2vec) #install_github("mukul13/rword2vec")
 library(RTextTools)
 library(SnowballC) # Paquete para minería de datos, agrupa aquellos términos que contienen la misma raíz
 library(tidytext)
@@ -24,6 +28,7 @@ library(tm)
 library(tseries)
 library(wesanderson)
 library(wordcloud)
+
 
 
 
